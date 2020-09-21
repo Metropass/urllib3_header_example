9d1cd419d94385bdeb541623b55c72f4c79cc211
Mo <58116522+Metropass@users.noreply.github.com>
2020-09-21 13:52:10 -0400

    Updated get_header_example.py
    Added a dummy call to the function. Replace this with an attribute that you'd like. If None, it prints the dict.

--- get_head_example.py
+++ get_head_example.py
@@ -1,13 +1,16 @@
 import urllib3
 import sys
-def header_example(attribute):
+def header_example(attribute=None):
     http = urllib3.PoolManager() # Creating the PoolManager object, can be created into multiple threads
     r = http.request('HEAD', 'https://www.google.ca') # Only Grabs HTTP Header Metadata, Stored as a Dictionary
     try:
-        print(r.headers[f'{attribute}']) # Matches 'attribute' key with it's content
+        if attribute is not None:
+            print(r.headers[f'{attribute}']) # Matches 'attribute' key with it's content
+        else:
+            print(r.headers)
     except KeyError:
         print('Bad Header Key!') #Exception if Key doesn't exist
     except:
         print("Unexpected Error: ", sys.exc_info()[0]) # Any Generic Error
 
-header_example() # Call to function
+header_example(attribute_argument_placeholder) # Call to function, requires an argument, which is the attribute that you want to display.
